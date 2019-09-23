63ad30603a9fe0e7f7c048efab11823dedb6d28b
zihao chen <chenzihao@buaa.edu.cn>
2019-09-23 17:15:02 +0800

    question2的解答

--- Solution.cpp
+++ Solution.cpp
@@ -10,3 +10,21 @@ void Solution::merge_q88(vector<int>& nums1,int m, vector<int>& nums2, int n){
     while(p1 >= 0) nums1[p--] = nums1[p1--];
     while(p2 >= 0) nums1[p--] = nums2[p2--]; 
 }
+
+ListNode* Solution::addTwoNumbers_q2(ListNode* l1, ListNode* l2) {
+        //将计算结果保存在l1中，本地操作
+        ListNode* result = l1;
+        ListNode* pre;              //记录前一个节点
+        int flag = 0;               //进位标志
+        while(l1||l2){
+            int sum;
+            l1? (pre=l1,l1=l1->next,sum = flag+pre->val):(pre->next = l2,pre = l2,sum=flag);
+            l2? (sum = l2->val + sum,l2 = l2->next):(pre->next=l1);
+            pre->val = sum%10;
+            sum>9?flag=1:flag=0;
+        }
+        if(l1==nullptr && l2==nullptr && flag==1){
+            pre->next = new ListNode(1);
+        }
+        return result;
+}
--- Solution.h
+++ Solution.h
@@ -2,9 +2,15 @@
 
 using std::vector;
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
 class Solution{
 
 public:
     void merge_q88(vector<int>& nums1, int m, vector<int>& nums2, int n);
-    
+    ListNode* addTwoNumbers_q2(ListNode* l1, ListNode* l2);
 };
