99c7e2f4606e5b941045f262876d02b6245dc19c
zihao chen <chenzihao@buaa.edu.cn>
2019-10-18 13:03:34 +0800

    尝试了一下使用单元测试工具，有点意思

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -4,4 +4,6 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
+add_test(myTEST266 "TEST415" "1" "2")
+set_tests_properties(myTEST266 PROPERTIES  _BACKTRACE_TRIPLES "E:/ACM/LeetCodeSource/CMakeLists.txt;30;add_test;E:/ACM/LeetCodeSource/CMakeLists.txt;0;")
 subdirs("SolutionSrc")
--- test415.cpp
+++ test415.cpp
@@ -1,8 +1,8 @@
 #include"Solution.h"
 
-int main(){
-    std::string s1="123";
-    std::string s2="32";
+int main(int argc, const char*argv[]){
+    std::string s1=argv[1];
+    std::string s2=argv[2];
     Solution s;
     std::string test=s.addString_q415(s1,s2);
     std::cout<<test<<std::endl;
