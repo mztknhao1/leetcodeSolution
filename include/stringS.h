99eaf98268256b80727f7dc0fb10f56913f20dea
mztknhao1 <chenzihao@buaa.edu.cn>
2019-12-01 20:31:00 +0800

    新增第8题题解，各种边界情况真的不容易考虑周全

--- include/stringS.h
+++ include/stringS.h
@@ -4,5 +4,6 @@
 #include "lib_head.h"
 
 int longestCommonSubsequence_q1143(string text1, string text2);
+int myAtoi_q8(string str);
 
 #endif
---
+++ testString.cpp
@@ -0,0 +1,8 @@
+#include "stringS.h"
+
+
+int main(){
+    string str=" ";
+    int result = myAtoi_q8(str);
+    return  0;
+}
