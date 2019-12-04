69d43fa93e10eaed7a2d91bd027a283916e057de
mztknhao1 <chenzihao@buaa.edu.cn>
2019-12-04 21:37:55 +0800

    很多时候，没有时间限制的情况下，回溯/暴力破解是个可行的选择

--- SolutionSrc/trackback.cpp
+++ SolutionSrc/trackback.cpp
@@ -67,6 +67,7 @@ vector<string> letterCombinations_q17(string digits){
     mp['2'] = "abc",mp['3'] = "def",mp['4']="ghi",mp['5']="jkl";
     mp['6'] = "mno",mp['7'] = "pqrs",mp['8']="tuv",mp['9']="wxyz";
     vector<string> result;
+    if(digits.size()==0) return result;
     string track;
     trackback_q17(digits,mp,track,result);
     return result;
---
+++ test/testTrack.cpp
@@ -0,0 +1,8 @@
+#include "traceback.h"
+
+
+int main(){
+    string number = "23";
+    vector<string> result =  letterCombinations_q17(number);
+    return 0;
+}
