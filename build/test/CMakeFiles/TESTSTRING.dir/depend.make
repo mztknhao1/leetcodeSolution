0bdd5433d636ea6373ab664cecf6d8b2197cd11a
zihao chen <chenzihao@buaa.edu.cn>
2019-12-05 21:52:32 +0800

    update .gitignore

--- build/test/CMakeFiles/TESTSTRING.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for TESTSTRING.
-# This may be replaced when dependencies are built.
--- build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/ACM/LeetCodeSource/test
-# Build directory: E:/ACM/LeetCodeSource/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
