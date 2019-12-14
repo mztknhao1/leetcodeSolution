a30d6cb19c61b2ef3562e76e25d5900760a48396
zihao chen <chenzihao@buaa.edu.cn>
2019-12-14 21:53:34 +0800

    新增第744题题解，这个题比较简单，但用二分法可以达到logN的时间复杂度

---
+++ include/bfs.h
@@ -0,0 +1,8 @@
+#ifndef __BFS
+#define __BFS
+
+#include "lib_head.h"
+
+char nextGreatestLetter_744(vector<char>& letters, char target);
+
+#endif
---
+++ test/testBfs.cpp
@@ -0,0 +1,7 @@
+#include "bfs.h"
+
+int main(){
+    vector<char> cs = {'n','c','f'};
+    char re = nextGreatestLetter_744(cs,'m');
+    return 0;
+}
